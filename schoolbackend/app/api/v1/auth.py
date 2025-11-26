1d51d3b7a9fcbeb4a343415de53895cf13863f70
Mr.T <terryrangel04@gmail.com>
2025-11-26 10:41:42 -0600

    Se arreglo error de shcema de estudainte

--- schoolbackend/app/api/v1/auth.py
+++ schoolbackend/app/api/v1/auth.py
@@ -12,11 +12,10 @@ def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), 
     db: Session = Depends(get_db)
 ):
-    # 1. Buscamos al usuario por email
-    # Nota: OAuth2PasswordRequestForm usa 'username' aunque nosotros usemos email
+    
     user = crud_user.get_user_by_email(db, email=form_data.username)
 
-    # 2. Verificamos si el usuario existe y si la contraseña coincide
+    
     if not user or not security.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -24,8 +23,6 @@ def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    # 3. Si todo es correcto, generamos el Token JWT
     access_token = security.create_access_token(data={"sub": user.email})
 
-    # 4. Devolvemos el token y el tipo
     return {"access_token": access_token, "token_type": "bearer"}
